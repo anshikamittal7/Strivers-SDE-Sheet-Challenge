b933e62c84986ee2c46f2e394fcb7fe805005c57
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2023-06-09 23:36:22 +0530

    Create pascalsTriangle.java

---
+++ Day 1/pascalsTriangle.java
@@ -0,0 +1,19 @@
+public class Solution {
+	public static ArrayList<ArrayList<Long>> printPascal(int n) {
+        // Write your code here.
+		ArrayList<ArrayList<Long>> res = new ArrayList<>();
+
+		for(int i = 0; i < n; i++){
+			res.add(new ArrayList<Long>());
+			for(int j = 0; j <= i; j++){
+				if(j == 0 || j == i){
+					res.get(i).add((long)1);
+				}
+				else{
+					res.get(i).add((res.get(i-1).get(j-1))+res.get(i-1).get(j));
+				}
+			}
+		}
+		return res;
+	}
+}
